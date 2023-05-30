b613614d2443d7a1f0e20aab6bccc56d3dac1968
Jefte Lopes <joao.pedro@moldsoft.com.br>
2023-05-30 09:42:49 -0300

    Trainee

---
+++ trainee/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["trainee/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ trainee/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"trainee/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
